d578d8b2851cf4e7437a1c18236e895ac568d02c
Ivy Ti <chivy1221@gmail.com>
2020-01-12 10:39:23 +0200

    first commit on beat machine 👨‍🦳

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'beatDemo';
+  sounds = "";
+  turnoff: boolean = false;
+  playSound(sound) {
+    
+    if (this.turnoff) {
+      this.sounds = sound + ' turned off \n '
+    }
+    else{
+      this.sounds = sound + ' ';
+    sound = "../assets/sound/" + sound + ".mp3";
+    sound && (new Audio(sound)).play()
+    }
+  }
+  
+  off(e) {
+    console.log(e);
+    this.turnoff = e.checked;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
