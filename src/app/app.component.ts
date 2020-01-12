4fcac386315c187b2c4fda53968d25a526e707ee
Ivy Ti <chivy1221@gmail.com>
2020-01-12 15:47:35 +0200

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,7 @@ export class AppComponent {
   title = 'beatDemo';
   sounds = "";
   turnoff: boolean = false;
-  playSound(sound) {
-    
-    if (this.turnoff) {
-      this.sounds = sound + ' turned off \n '
-    }
-    else{
-      this.sounds = sound + ' ';
-    sound = "../assets/sound/" + sound + ".mp3";
-    sound && (new Audio(sound)).play()
-    }
-  }
+ 
   
   off(e) {
     console.log(e);
